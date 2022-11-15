2c4045a10ab75b8a15eea6c2f364de2f74c24d1b
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-11-16 02:00:22 +0600

    mongo db add

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middle ware 
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1ndgjy2.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const appointmentOptionCollection = client.db('doctorsHome').collection('appointmentOption')
+
+        app.get('/appointmentOption', async (req, res) => {
+            const query = {};
+            const options = await appointmentOptionCollection.find(query).toArray();
+            res.send(options);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/', async (req, res) => {
+    res.send('doctors home server is running');
+})
+
+app.listen(port, () => console.log(`doctors portal running on : ${port}`))
